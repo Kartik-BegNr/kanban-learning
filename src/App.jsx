2e588eb8ec8b7a56c67e5953d5da70b327d70c0b
Kartik <ksraihal@gmail.com>
2025-08-14 20:12:46 +0530

    project setup , vite + tailwind CDN

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App(){
+  return(
+    <div>
+      <h1 className="text-2xl font-bold text-center mt-8">Hello Kanban</h1>
+    </div>
+  )
+}
